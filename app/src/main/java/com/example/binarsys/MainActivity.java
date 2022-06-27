a2d6bab2a9500be598b6aa842c393eba23a015ee
Maximuss1 <maxim.July16@gmail.com>
2022-06-27 18:06:19 +0200

    start

--- app/src/main/java/com/example/binarsys/MainActivity.java
+++ app/src/main/java/com/example/binarsys/MainActivity.java
@@ -72,7 +72,6 @@ public class MainActivity extends AppCompatActivity {
             bin_sms+=" ";
         }
 
-
         String finalBin_sms = bin_sms;
         System.out.println(recivedText + " " + finalBin_sms);
         getBinaryCodeText.setOnClickListener(new View.OnClickListener() {
